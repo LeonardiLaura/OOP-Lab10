db85e153b5c3b99bc0621f7a762b3207e1f06264
unknown <laura.leonardi9@studio.unibo.it>
2020-12-04 18:45:25 +0100

    es03

--- src/it/unibo/oop/lab/reactivegui03/AnotherConcurrentGUI.java
+++ src/it/unibo/oop/lab/reactivegui03/AnotherConcurrentGUI.java
@@ -13,33 +13,33 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
-import it.unibo.oop.lab.reactivegui02.ConcurrentGUI;
-//import it.unibo.oop.lab.reactivegui02.ConcurrentGUI.Agent;
-
 public class AnotherConcurrentGUI {
     private static final long serialVersionUID = 1L;
     private static final double WIDTH_PERC = 0.2;
     private static final double HEIGHT_PERC = 0.1;
     private final JFrame frame = new JFrame();
     private final JLabel label = new JLabel("0");
+    private final JPanel panel = new JPanel();
+    private final JButton bUp = new JButton("up");
+    private final JButton bDown = new JButton("down");
+    private final JButton bStop = new JButton("stop");
+    private final Agent agent = new Agent();
 
     public AnotherConcurrentGUI() {
         final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        final JPanel panel = new JPanel();
         panel.add(label);
-        final JButton bUp = new JButton("up");
         panel.add(bUp);
-        final JButton bDown = new JButton("down");
         panel.add(bDown);
-        final JButton bStop = new JButton("stop");
         panel.add(bStop);
         frame.getContentPane().add(panel);
         frame.setVisible(true);
 
-        final Agent agent = new Agent();
         new Thread(agent).start();
+
+        final TimeOutAgent agent2 = new TimeOutAgent();
+        new Thread(agent2).start();
 
         bUp.addActionListener(new ActionListener() {
             public void actionPerformed(final ActionEvent e) {
@@ -69,12 +69,12 @@ public class AnotherConcurrentGUI {
         public void run() {
             while (!this.stop) {
                 try {
-                    SwingUtilities.invokeAndWait(() -> AnotherConcurrentGUI.this.label.setText(Integer.toString(this.counter)));
                     if (this.dirUp) {
                         this.counter++;
                     } else {
                         this.counter--;
                     }
+                    SwingUtilities.invokeAndWait(() -> AnotherConcurrentGUI.this.label.setText(Integer.toString(this.counter)));
                     Thread.sleep(100);
                 } catch (InvocationTargetException | InterruptedException ex) {
                     ex.printStackTrace();
@@ -90,6 +90,21 @@ public class AnotherConcurrentGUI {
         }
         public void changeToDown() {
             this.dirUp = false;
+        }
+    }
+    private class TimeOutAgent implements Runnable {
+        private static final int TIME = 10_000;
+        @Override
+        public void run() {
+           try {
+               Thread.sleep(TIME);
+               AnotherConcurrentGUI.this.agent.stopCounting();
+               AnotherConcurrentGUI.this.bDown.setEnabled(false);
+               AnotherConcurrentGUI.this.bUp.setEnabled(false);
+               AnotherConcurrentGUI.this.bStop.setEnabled(false);
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
         }
     }
 }
